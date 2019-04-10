f1a3a4e8e06886bfb42c41960d10b5134c20cce1
Kage18 <18kushaljain@gmail.com>
2019-04-10 14:57:22 +0530

    Final

---
+++ Accident_sensor.ino
@@ -0,0 +1,328 @@
+#include<SoftwareSerial.h>
+SoftwareSerial Serial1(4,5); //make RX arduino line is pin 2, make TX arduino line is pin 3.
+SoftwareSerial gps(11,12);
+#define x A1
+#define y A2
+#define z A3
+#define RX 2
+#define TX 3
+String AP = "Kushal";
+String PASS = "aslkqwpo";
+String API = "12GCNDPX3DJBSHCM";
+String HOST = "api.thingspeak.com";
+String PORT = "80";
+String field1 = "field1";
+String field2 = "field2";
+int countTrueCommand;
+int countTimeCommand; 
+boolean found = false; 
+int valSensor = 1;
+SoftwareSerial esp8266(RX,TX); 
+int xsample=0;
+int ysample=0;
+int zsample=0;
+int sensePin = A0;  
+int sensorInput;
+double temp; 
+
+#define samples 10
+ 
+#define minVal -200
+#define MaxVal 200
+
+int i=0,k=0;
+int  gps_status=0;
+float latitude=0; 
+float logitude=0;                       
+String Speed="";
+String gpsString="";
+char *test="$GPRMC";
+
+void initModule(String cmd, char *res, int t)
+{
+  while(1)
+  {
+    Serial.println(cmd);
+    Serial1.println(cmd);
+    delay(100);
+    while(Serial1.available()>0)
+    {
+       if(Serial1.find(res))
+       {
+        Serial.println(res);
+        delay(t);
+        return;
+       }
+
+       else
+       {
+        Serial.println("Error");
+       }
+    }
+    delay(t);
+  }
+}
+
+void setup() 
+{
+  Serial1.begin(9600);
+  Serial.begin(9600);
+ Serial.println("Initializing....");
+  initModule("AT","OK",1000);
+  initModule("ATE1","OK",1000);
+  initModule("AT+CPIN?","READY",1000);  
+  initModule("AT+CMGF=1","OK",1000);     
+  initModule("AT+CNMI=2,2,0,0,0","OK",1000);  
+  Serial.println("Initialized Successfully");
+  
+  Serial.begin(9600);
+  esp8266.begin(115200);
+  sendCommand("AT",5,"OK");
+  sendCommand("AT+CWMODE=1",5,"OK");
+  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
+  for(int i=0;i<samples;i++)
+  {
+    xsample+=analogRead(x);
+    ysample+=analogRead(y);
+    zsample+=analogRead(z);
+  }
+
+  xsample/=samples;
+  ysample/=samples;
+  zsample/=samples;
+
+  Serial.println(xsample);
+  Serial.println(ysample);
+  Serial.println(zsample);
+  delay(1000);
+  gps.begin(9600);
+  get_gps();
+  show_coordinate();
+  delay(2000);
+  Serial.println("System Ready..");
+}
+
+void loop() 
+{
+    int value1=analogRead(x);
+    int value2=analogRead(y);
+    int value3=analogRead(z);
+
+    int xValue=xsample-value1;
+    int yValue=ysample-value2;
+    int zValue=zsample-value3;
+    sensorInput = analogRead(A0);    
+    temp = (double)sensorInput / 1024;
+    temp = temp * 5; 
+    temp = temp - 0.5;    
+    temp = temp * 10; 
+
+  Serial.print("Current Temperature: ");
+  Serial.println(temp);
+    Serial.print("x=");
+    Serial.println(xValue);
+    Serial.print("y=");
+    Serial.println(yValue);
+    Serial.print("z=");
+    Serial.println(zValue);
+    
+    if(xValue < minVal || xValue > MaxVal  || yValue < minVal || yValue > MaxVal  || zValue < minVal || zValue > MaxVal|| temp > 75 )
+    {
+      gps.begin(9600);
+      get_gps();
+      show_coordinate();
+
+      Serial.println("Sending SMS");
+      Send();
+      //gpslogging();
+      esp8266.begin(115200);
+      
+       String getData1 = "GET /update?api_key="+ API +"&"+ field1 +"="+String(latitude)+"&"+ field2 +"="+String(logitude);
+       sendCommand("AT+CIPMUX=1",5,"OK");
+       sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
+       delay(3000);
+       sendCommand("AT+CIPSEND=0," +String(getData1.length()+4),4,">");
+       esp8266.println(getData1);delay(1500);countTrueCommand++;
+       sendCommand("AT+CIPCLOSE=0",5,"OK");
+       Serial.println("SMS Sent");
+       delay(2000);
+    }        
+}
+
+void sendCommand(String command, int maxTime, char readReplay[]) {
+  Serial.print(countTrueCommand);
+  Serial.print(". at command => ");
+  Serial.print(command);
+  Serial.print(" ");
+  while(countTimeCommand < (maxTime*1))
+  {
+    esp8266.println(command);//at+cipsend
+    if(esp8266.find(readReplay))//ok
+    {
+      found = true;
+      break;
+    }
+  
+    countTimeCommand++;
+  }
+  
+  if(found == true)
+  {
+    Serial.println("OYI");
+    countTrueCommand++;
+    countTimeCommand = 0;
+  }
+  
+  if(found == false)
+  {
+    Serial.println("Fail");
+    countTrueCommand = 0;
+    countTimeCommand = 0;
+  }
+  
+  found = false;
+ }
+void gpsEvent()
+{
+  gpsString="";
+  while(1)
+  {
+   while (gps.available()>0)            //Serial incoming data from GPS
+   {
+    char inChar = (char)gps.read();
+     gpsString+= inChar;                    //store incoming data from GPS to temparary string str[]
+     i++;
+    Serial.print(inChar);
+    //Serial.print("");
+     if (i < 7)                      
+     {
+      if(gpsString[i-1] != test[i-1])         //check for right string
+      {
+        i=0;
+        gpsString="";
+      }
+     }
+    if(inChar=='\r')
+    {
+     if(i>60)
+     {
+       gps_status=1;
+       break;
+     }
+     else
+     {
+       i=0;
+     }
+    }
+  }
+   if(gps_status)
+    break;
+  }
+}
+
+void get_gps()
+{
+   gps_status=0;
+   int x=0;
+   while(gps_status==0)
+   {
+    gpsEvent();
+    int str_lenth=i;
+    coordinate2dec();
+    i=0;x=0;
+    str_lenth=0;
+   }
+}
+
+void show_coordinate()
+{
+    Serial.print("Latitude:");
+    Serial.println(latitude);
+    Serial.print("Longitude:");
+    Serial.println(logitude);
+    Serial.print("Speed(in knots)=");
+    Serial.println(Speed);
+    delay(2000);
+
+}
+
+void coordinate2dec()
+{
+  String lat_degree="";
+    for(i=19;i<=20;i++)         
+      lat_degree+=gpsString[i];
+      
+  String lat_minut="";
+     for(i=21;i<=27;i++)         
+      lat_minut+=gpsString[i];
+
+  String log_degree="";
+    for(i=32;i<=34;i++)
+      log_degree+=gpsString[i];
+
+  String log_minut="";
+    for(i=35;i<=41;i++)
+      log_minut+=gpsString[i];
+    
+    Speed="";
+    for(i=46;i<51;i++)          //extract longitude from string
+      Speed+=gpsString[i];
+      
+     float minut= lat_minut.toFloat();
+     minut=minut/60;
+     float degree=lat_degree.toFloat();
+     latitude=degree+minut;
+     
+     minut= log_minut.toFloat();
+     minut=minut/60;
+     degree=log_degree.toFloat();
+     logitude=degree+minut;
+}
+
+void Send()
+{ 
+   Serial1.println("AT");
+   delay(500);
+   serialPrint();
+   Serial1.println("AT+CMGF=1");
+   delay(500);
+   serialPrint();
+   Serial1.print("AT+CMGS=");
+   Serial1.print('"');
+   Serial1.print("9340143387");
+   Serial1.println('"');
+   delay(500);
+   serialPrint();
+   Serial1.print("Temp:");
+   Serial1.println(temp);
+   delay(500);
+   serialPrint();
+   Serial1.print("Latitude:");
+   Serial1.println(latitude);
+   delay(500);
+   serialPrint();
+   Serial1.print(" longitude:");
+   Serial1.println(logitude);
+   delay(500);
+   serialPrint();
+   Serial1.print(" Speed:");
+   Serial1.print(Speed);
+   Serial1.println("Knots");
+   delay(500);
+   serialPrint();
+   Serial1.print("http://maps.google.com/maps?&z=15&mrt=yp&t=k&q=");
+   Serial1.print(latitude,6);
+   Serial1.print("+");
+   Serial1.print(logitude,6);
+   Serial1.write(26);
+   delay(2000);
+   serialPrint();
+}
+
+void serialPrint()
+{
+  while(Serial1.available()>0)
+  {
+    Serial.print(Serial1.read());
+  }
+}
